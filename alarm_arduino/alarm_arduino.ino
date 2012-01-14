73104b3604acfd495f7af59c1c37d661b7fd809c
Phil Cluff <philcluff@googlemail.com>
2012-01-14 23:58:15 +0000

    Commenting and cleanup of the arduino code

--- alarm_arduino/alarm_arduino.ino
+++ alarm_arduino/alarm_arduino.ino
@@ -1,9 +1,15 @@
+// Poll a reed switch and send its current state via serial.
+// The reed switch in this case is closed when the magnet is
+// within range of the sensor.
+
+// Configuration
 int warn_led = 13;
 int ok_led = 11;
 int serialSpeed = 9600;
 int sensor = 7;
 int val = 0;
 
+// Setup the I/O pins and set the serial rate
 void setup() {
   pinMode(ok_led, OUTPUT);
   pinMode(warn_led, OUTPUT);
@@ -11,18 +17,23 @@ void setup() {
   Serial.begin(9600);
 }
 
+// Looooooooop!
 void loop() {
     delay(500); 
     val = digitalRead(sensor);
-    
-    if(val == HIGH) {
+ 
+   // If the switch is closed (Door closed)
+   if(val == HIGH) {
       digitalWrite(warn_led, LOW);
       digitalWrite(ok_led, HIGH);
       Serial.println("OK");
     }
+    
+   // If the switch is open (Door open)
     else {
       digitalWrite(warn_led, HIGH);
       digitalWrite(ok_led, LOW);
       Serial.println("WARN");
     }
+
 }
